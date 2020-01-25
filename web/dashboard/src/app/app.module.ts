a08577ff35e5298123bb658901b1c1be9c852275
Dipan Roy <dipanroy12@gmail.com>
2020-01-26 01:33:39 +0530

    Added Login Layout Component

--- web/dashboard/src/app/app.module.ts
+++ web/dashboard/src/app/app.module.ts
@@ -5,15 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
-import { SettingsComponent } from './modules/settings/settings.component';
-import { ControlComponent } from './modules/control/control.component';
-import { ContactComponent } from './modules/contact/contact.component';
-import { HelpComponent } from './modules/help/help.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ApixuService } from './modules/apixu.service';
-import { SetupComponent } from './layouts/setup/setup.component';
-import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
