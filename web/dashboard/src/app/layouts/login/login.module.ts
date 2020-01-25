acb18c55e78909ea54b9a05ca2b779b0f3e6e999
Dipan Roy <dipanroy12@gmail.com>
2020-01-26 01:45:36 +0530

    Finally Managed to set Login & Setup Module

--- web/dashboard/src/app/layouts/login/login.module.ts
+++ web/dashboard/src/app/layouts/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -9,7 +11,9 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    BrowserModule
   ]
 })
 export class LoginModule { }
--- web/dashboard/src/app/layouts/setup/setup.module.ts
+++ web/dashboard/src/app/layouts/setup/setup.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { SetupComponent } from './setup.component';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +13,6 @@ import { BrowserModule } from '@angular/platform-browser';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule,
     BrowserModule
   ]
 })
