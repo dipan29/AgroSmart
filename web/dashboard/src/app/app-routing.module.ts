f3cc3ee565cb9d02e5ff4bfd88091337f8c62dc1
Dipan Roy <dipanroy12@gmail.com>
2020-01-25 12:18:44 +0530

    Added Componenets & Modules

--- web/dashboard/src/app/app-routing.module.ts
+++ web/dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { StatsComponent } from './modules/stats/stats.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent
+  }, {
+    path: 'stats',
+    component: StatsComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ web/dashboard/src/app/layouts/default/default.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { StatsComponent } from 'src/app/modules/stats/stats.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    StatsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ]
+})
+export class DefaultModule { }
