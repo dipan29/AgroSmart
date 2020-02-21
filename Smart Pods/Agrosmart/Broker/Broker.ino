3068bf359fd3cfcdd991260c9c97a25c9287d463
xritzx <ritankarpaul47@gmail.com>
2020-02-21 16:42:18 +0530

    Added Light Sensor

--- Smart Pods/Agrosmart/Broker/Broker.ino
+++ Smart Pods/Agrosmart/Broker/Broker.ino
@@ -1,9 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <DNSServer.h>
+#include <Servo.h>
+#include <DHT.h>
 
 #include <TaskScheduler.h>
-#include <Servo.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include "Agrosmart.h"
@@ -11,20 +12,21 @@
 #define RELAY_2 D2
 #define RELAY_3 D1
 #define DHTPIN D7
+#define DHTTYPE DHT11
 #define SERVO_PIN D8
 
 //Multiplexer Selection Line 
 #define S0 D3
 #define S1 D4
 
-#define SOIL_MOISTURE A0
+#define ANALOG_SENSOR A0
 
 static String URL_POST = "http://192.168.0.108:7988/node_data/send";
-static String URL_GET = "http://192.168.0.108:7988/controller/69696";
-static String CONTROLLER_ID = "controller: "+ESP.getChipId();
+static String URL_GET = "http://192.168.0.108:7988/controller/"+String(ESP.getChipId());
 
 AgroSmart agrosmart;
 Servo servo;
+DHT dht(DHTPIN, DHTTYPE);
 
 /*====== Callbacks associated with TaskScheduling ======*/
 void getRelayState();
@@ -37,8 +39,14 @@ Scheduler TaskManager;
 
 /*====== Getting Access Point IP for WiFi Config ====== */
 void APModeCallback(WiFiManager* manager) {
-  Serial.println("Entered config mode..");
+  Serial.println("Entered config mode, Please login with your WiFi/AP Credentials..");
   Serial.println(WiFi.softAPIP());
+}
+
+/*====== Setting Multiplexer Selections ====== */
+void setMultiplexer(int s1, int s0){
+  digitalWrite(S1, s1==0?LOW:HIGH);
+  digitalWrite(S0, s0==0?LOW:HIGH);
 }
 
 /*====== Setting up the Relay Channels by the response ====== */
@@ -51,8 +59,6 @@ void getRelayState(){
   String r2 = doc["relay2"];
   String r3 = doc["relay3"];
   String s1 = doc["servo1"];
-  Serial.println(r2);
-  Serial.println(r3);
   digitalWrite(RELAY_2, r2.toInt()?HIGH:LOW);
   digitalWrite(RELAY_3, r3.toInt()?HIGH:LOW);
   servo.write(s1.toInt());
@@ -62,28 +68,37 @@ void getRelayState(){
 void sendSensorData(){
     StaticJsonDocument<1000> doc;
     JsonObject data = doc.to<JsonObject>();
-    
     String JSONmessageBuffer;
+    
     data["deviceID"]=String(ESP.getChipId());
-    data["temperature"]="31";
-    data["humidity"]="86";
-    data["moisture"]=String((analogRead(SOIL_MOISTURE)*1000)/1024.0);
+    data["temperature"]=String(dht.readTemperature());
+    data["humidity"]=String(dht.readHumidity());
+    
+    // Reading the Sensor at (S1,S0)=(0,0)
+    setMultiplexer(0,0);
+    data["moisture"]=String((analogRead(ANALOG_SENSOR)*100)/1024.0);
+    
+    // Reading the Sensor at (S1,S0)=(0,1)
+    setMultiplexer(0,1);
+    data["solarIntensity"]=String((analogRead(ANALOG_SENSOR)*100)/1024.0);
 
     //Stringifying JSON Doc
     serializeJsonPretty(doc, JSONmessageBuffer);  
     Serial.println("........Printing data.........");
     Serial.println(JSONmessageBuffer);
-    
     Serial.println(agrosmart.post_data(JSONmessageBuffer, URL_POST));
 }
 
 void setup() {
  
   Serial.begin(115200);    //Serial connection
+  dht.begin();
 
   servo.attach(SERVO_PIN);
   pinMode(RELAY_2, OUTPUT);
   pinMode(RELAY_3, OUTPUT);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
   
   /*======= WiFi Manager Setup =======*/
   WiFiManager wifiManager;
@@ -96,8 +111,9 @@ void setup() {
     ESP.reset();
     delay(5000);
   }
+  
   Serial.print(WiFi.localIP());
-  Serial.println(" Connected");
+  Serial.println("Connected");
   
   /*======= Task init and Schedules =======*/
   Serial.println("\n Tasking the Device with GET and POST with 2s and 10s Intervals");
