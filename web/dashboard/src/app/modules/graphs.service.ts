86db1198187ef2476492cf558cf30edad90a7872
xritzx <ritankarpaul47@gmail.com>
2020-02-26 19:31:28 +0530

    Added current date forecasting

--- web/dashboard/src/app/modules/graphs.service.ts
+++ web/dashboard/src/app/modules/graphs.service.ts
@@ -15,7 +15,7 @@ export class GraphsService {
   constructor(private cookieService: CookieService, private http: HttpClient) { }
 
   getDayAverageData() {
-    const propertyID = this.cookieService.get('propertyId') || '6-YaNQcL';
+    const propertyID = this.cookieService.get('propertyId') || 'qz6l93ia';
     if (propertyID) {
       console.log('Data for Property ID - ' + this.cookieService.get('propertyId'));
       return this.http.post<GraphData>('/api/graphs/data', {
@@ -24,19 +24,14 @@ export class GraphsService {
     }
   }
 
-  getNodeData(parameter, startDate, endDate) {
-      const ygraph = [];
-      const xgraph = [];
-      this.getDayAverageData().subscribe(data => {
-        ygraph.push(data[0].sensorGraph);
-        xgraph.push(data[0].timeStamps);
-        console.log(ygraph[0]);
-        console.log(xgraph[0]);
-      });
-      console.log('Printing Data');
-
-      return [ygraph[0], xgraph[0]];
+  getDateData() {
+    const getDate = new Date().toString().substring(4, 15); // to Format Feb 21 2020
+    const propertyID = this.cookieService.get('propertyId') || 'qz6l93ia';
+    console.log(`Getting Data on ${getDate}`);
+    return this.http.post<GraphData>('/api/graphs/date', {
+      propertyID,
+      getDate
+    });
   }
-
 
 }
