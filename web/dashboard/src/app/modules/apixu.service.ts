e119a0d16a0239d95d8735d76fb47e233f75e26e
Dipan Roy <dipanroy12@gmail.com>
2020-02-27 17:47:10 +0530

    Added OpenWeatherMap

--- web/dashboard/src/app/modules/apixu.service.ts
+++ web/dashboard/src/app/modules/apixu.service.ts
@@ -11,9 +11,15 @@ export class ApixuService {
 
   getWeather(location) {
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=1c8a0ab1a2b74a85182ff799b24932a1&query=' + location
+      'http://api.openweathermap.org/data/2.5/weather?appid=913625056cfd266d73c8feb4ee18924a&q=' + location
     );
   }
+  getForecast(location) {
+    return this.http.get(
+      'http://api.openweathermap.org/data/2.5/forecast?appid=913625056cfd266d73c8feb4ee18924a&q=' + location
+    );
+  }
+
   getAirQuality(location){
     return this.http.get(
       'http://api.waqi.info/feed/'+ location + '/?token=7cc2cc0a80ff18f1ac5387c27d6d83f051999a5b'
